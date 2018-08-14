a38fbe030a074d4ae94b0b2dd4708a058e3e5d3a
Syed Namdar Hussain <syednh110@gmail.com>
2018-08-15 01:14:00 +0530

    first commit

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+# YouTube Video: https://www.youtube.com/watch?v=rhBZqEWsZU4
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "804258028526522368-0bHL5szbFKM5XXXXXXXXXXXXXX"
+ACCESS_TOKEN_SECRET = "QHH8B9TlFx7cQalTdr5dYt3XXXXXXXXXXXx"
+CONSUMER_KEY = "tD7EQnr0oHClPVZu85RVnTutGXXXXXXXXXXXXXXXX"
+CONSUMER_SECRET = "SlwfMY3GcQiuY9M2eVitbHqXXXXXXXXXXXXXXX"
+
